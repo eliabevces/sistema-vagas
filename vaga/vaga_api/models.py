6d4fdec1daff1993e2d86ebb16923c2b7f867f94
Eliabevces <eliabevces@ufu.br>
2023-07-02 13:19:08 -0300

    Fix: User model

--- vaga/vaga_api/models.py
+++ vaga/vaga_api/models.py
@@ -16,10 +16,13 @@ class User(AbstractUser):
 
 
 class Candidato(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, related_name='student')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, related_name='candidato')
     pretensão_salarial = models.DecimalField(max_digits= 5, decimal_places=2)
     experiencia = models.CharField(max_length = 300)
     escolaridade = models.PositiveSmallIntegerField(choices=ESCOLARIDADES_CHOICES, default=1, blank=False, null=False)
+class Empresa(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, related_name='empresa')
+
 
 
 class Vaga(models.Model):
@@ -37,6 +40,8 @@ class Vaga(models.Model):
     timestamp = models.DateTimeField(auto_now_add = True, auto_now = False, blank = True)
     updated = models.DateTimeField(auto_now = True, blank = True)
     candidatos = models.ManyToManyField(Candidato, blank = True)
+    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
+
 
     def __str__(self):
         return self.nome
